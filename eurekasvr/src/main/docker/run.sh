e4029f80960176dc97468f4f551d5f3797fce4a8
Iegor Maiorov <maiorov.prog@gmail.com>
2017-12-09 23:48:40 +0200

    fixed few typos

--- eurekasvr/src/main/docker/run.sh
+++ eurekasvr/src/main/docker/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 echo "#################################################"
 echo "##  Starting Eureka Service Discovery Server  ##"
 echo "################################################"
-
 java -jar /usr/local/eurekasvr/@project.build.finalName@.jar
--- eurekasvr/src/main/java/org/home/eurekasvr/EurekaServerApplication.java
+++ eurekasvr/src/main/java/org/home/eurekasvr/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekasvrApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekasvrApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
