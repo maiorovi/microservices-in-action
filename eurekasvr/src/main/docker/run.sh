995f8f3bdeb66c6458fd55b0d6584381dc1a3ed9
Iegor Maiorov <maiorov.prog@gmail.com>
2017-12-09 23:41:09 +0200

    added eureka server

---
+++ eurekasvr/src/main/docker/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "#################################################"
+echo "##  Starting Eureka Service Discovery Server  ##"
+echo "################################################"
+
+java -jar /usr/local/eurekasvr/@project.build.finalName@.jar
